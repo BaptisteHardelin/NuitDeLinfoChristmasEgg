72267f243310059e6a00824d9f72e86c3d8ebb93
Baptiste Hardelin <baptiste.hardelin.etu@univ-lille.fr>
2021-12-03 00:37:57 +0100

    commit

--- script/script.js
+++ script/script.js
@@ -1,14 +1,39 @@
 const rescueBoat = document.getElementById('rescueBoat');
 const brokenBoat = document.getElementById('brokenBoat');
+const rock = document.getElementById('rock');
+let restartCheck = document.getElementById('restartCheck');
+let boutonEnvoyer = document.getElementById('restart');
+let para = document.createElement('p');
+let gameOver = false;
 
-function jump() {
-    if(rescueBoat.classList != 'jump') {
-        // On ajoute au rescueBoat notre animation
-        rescueBoat.classList.add('jump');
-        setTimeout(function() {
-            rescueBoat.classList.remove('jump');
-        }, 300);
+function jump(event) {
+    var jump = event.keyCode;
+    if(jump == 32) {
+        if(rescueBoat.classList != 'jump') {
+            // On ajoute au rescueBoat notre animation
+            rescueBoat.classList.add('jump');
+            setTimeout(function() {
+                rescueBoat.classList.remove('jump');
+            }, 300);
+        }
     }
+}
+
+
+function addBlock(element) {
+    brokenBoat.classList.add(element);
+}
+
+function block(element) {
+    brokenBoat.classList.remove(element);
+}
+
+addBlock();
+
+function fGameOver(element) {
+    gameOver = true;
+    block(element);
+    para.textContent = 'Vous avez perdu !';
 }
 
 let isAlive = setInterval(function() {
@@ -16,17 +41,30 @@ let isAlive = setInterval(function() {
     let boatTop = parseInt(window.getComputedStyle(rescueBoat).getPropertyValue('top'));
     // On la position courante du rescueBoat en X
     let brokenBoatLeft = parseInt(window.getComputedStyle(brokenBoat).getPropertyValue('left'));
+    let rockLeft = parseInt(window.getComputedStyle(rock).getPropertyValue('left'));
 
     // On regarde si les 2 se touchent
     if(brokenBoatLeft < 50 && brokenBoatLeft < 0 && boatTop >= 140) {
-        alert('Game over !');
+        // On arrête l'animation du bateau
+        fGameOver('brokenBoatAnim');
     }
 
-    console.log(brokenBoatLeft);
-    console.log(boatTop);
+    if(rockLeft < 50 && rockLeft < 0 && boatTop >= 140) {
+        fGameOver('rockAnim');
+    }
 }, 10);
 
-
 document.addEventListener('keydown', function(event) {
-    jump();
+    jump(event);
 });
+
+boutonEnvoyer.addEventListener('click', function() {
+    if(restartCheck.value == 'oui') {
+        addBlock('brokenBoatAnim');
+        //addBlock('rockAnim');
+        para.textContent = '';
+    }
+});
+
+
+document.body.appendChild(para);
